71152bc0e366c3ecd310c5334c4cb78e3a7e3f82
Mohith kumar <murugan28aug@gmail.com>
2023-06-24 22:06:34 +0530

    ui

--- components/Home.js
+++ components/Home.js
@@ -60,7 +60,7 @@ const products = [
   const Home = () => {
     return (
         <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-10 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6  lg:max-w-7xl lg:px-8">
       
   
           <div className="mt-6 grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
